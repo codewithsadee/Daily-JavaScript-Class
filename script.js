836b4931f9f17004920cf4c9947b048e88799bc4
Sadee <mohammadsadee24@gmail.com>
2021-04-06 19:43:23 +0600

    Anisul islam function class task

---
+++ script.js
@@ -0,0 +1,20 @@
+function addition(x, y) {
+  var result = x + y;
+  document.write("sum = " + result + "<br>");
+}
+function subtraction(x, y) {
+  var result = x - y;
+  document.write("sub = " + result + "<br>");
+}
+function multiplication(x, y) {
+  var result = x * y;
+  document.write("mul = " + result + "<br>");
+}
+function divition(x, y) {
+  var result = x / y;
+  document.write("divition = " + result + "<br>");
+}
+addition(30, 100);
+subtraction(20, 10);
+multiplication(5, 5);
+divition(10, 5);
