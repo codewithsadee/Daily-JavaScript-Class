ea211c8b4b291a79471307a62c2d001ec05fd8cb
Sadee <mohammadsadee24@gmail.com>
2021-04-08 17:52:16 +0600

    adding modulas fanction --> Editor: Sadee

--- script.js
+++ script.js
@@ -10,11 +10,15 @@ function multiplication(x, y) {
   var result = x * y;
   document.write("mul = " + result + "<br>");
 }
-function divition(x, y) {
+function division(x, y) {
   var result = x / y;
   document.write("divition = " + result + "<br>");
+}
+function modulas(x, y) {
+  return x % y;
 }
 addition(30, 100);
 subtraction(20, 10);
 multiplication(5, 5);
-divition(10, 5);
+division(10, 5);
+document.write("modulas = " + modulas(2, 1));
