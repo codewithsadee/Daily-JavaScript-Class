8df04b19976310df71f0f7a1cfebbf28b332c6cf
Sadee <mohammadsadee24@gmail.com>
2021-04-11 19:35:37 +0600

    function self invoking

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-function addition(x, y) {
-  var result = x + y;
-  document.write("sum = " + result + "<br>");
-}
-function subtraction(x, y) {
-  var result = x - y;
-  document.write("sub = " + result + "<br>");
-}
-function multiplication(x, y) {
-  var result = x * y;
-  document.write("mul = " + result + "<br>");
-}
-function division(x, y) {
-  var result = x / y;
-  document.write("divition = " + result + "<br>");
-}
-function modulas(x, y) {
-  return x % y;
-}
-addition(30, 100);
-subtraction(20, 10);
-multiplication(5, 5);
-division(10, 5);
-document.write("modulas = " + modulas(2, 1));
+// This function calling self
+(function selfInvoke() {
+  document.write("I'm Self invoke");
+})();
