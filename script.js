96a3e09136bcabe33d571de196a53635f7a73ae8
Sadee <mohammadsadee24@gmail.com>
2021-04-17 23:26:17 +0600

    Array declaration with Array() keyword

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function array() {
-  var numbers = [0, 27, 30, 40, 59, 60, 73, 80, 90];
-  numbers.push(99);
-  for (var x = 1; x < 10; x++) {
-    if (numbers[x] % 2 == 0) {
-      numbers[0] += numbers[x];
-      document.write(
-        'This is Even numbers <span class="even">' + numbers[x] + "</span><br>"
-      );
-    } else {
-      document.write(
-        'This is Odd numbers <span class="odd">' + numbers[x] + "</span><br>"
-      );
-    }
-  }
-  document.write('All Even equal to = <span class="even">' + numbers[0] + "</span>");
-}
+// Array declaration with Array() keyword
+var newArray = new Array();
 
-array();
+// Array value storing
+newArray[0] = "Tokee Abdullah";
+newArray[1] = "20 years old";
+newArray[2] = "Bangladesh";
+
+// Printing Array value
+document.write(
+  "Name : " +
+    newArray[0] +
+    "<br>" +
+    "Age : " +
+    newArray[1] +
+    "<br>" +
+    "Country : " +
+    newArray[2] + '<br>'
+);
