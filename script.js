77ff1096c4d519f9108431b69facee292990c078
Sadee <mohammadsadee24@gmail.com>
2021-04-17 23:46:14 +0600

    Array Declaration with [third bracket]

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-// Array declaration with Array() keyword
-var newArray = new Array();
+// Simple Array Declaration
+var newArray = ["Bangladesh", "Pakistan", "Sri Lanka", "Nepal"];
 
-// Array value storing
-newArray[0] = "Tokee Abdullah";
-newArray[1] = "20 years old";
-newArray[2] = "Bangladesh";
+// Printing Array Value
+console.log(newArray);
 
-// Printing Array value
-document.write(
-  "Name : " +
-    newArray[0] +
-    "<br>" +
-    "Age : " +
-    newArray[1] +
-    "<br>" +
-    "Country : " +
-    newArray[2] + '<br>'
-);
+// Indivisual value print
+console.log(newArray[0]);
+console.log(newArray[1]);
+console.log(newArray[2]);
+console.log(newArray[3]);
