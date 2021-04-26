040b0b94292fc33fe97d121bd0c2780841e3d8b4
Mohammad Sadee <mohammadsadee24@gmail.com>
2021-04-27 03:33:01 +0600

    Object Class 1

--- script.js
+++ script.js
@@ -3,47 +3,28 @@
 const ps = require("prompt-sync");
 const prompt = ps();
 
-//* creating array with some numbers
-var someNumbers = [
-  50,
-  1,
-  3,
-  4,
-  7,
-  9,
-  15,
-  13,
-  16,
-  11,
-  20,
-  28,
-  24,
-  23,
-  21,
-  37,
-  39,
-  31,
-  34,
-  49,
-  45,
-  42,
-  46,
-];
+var country1 = {
+  name: "Bangladesh",
+  currency: "Taka",
+  population: "161,376,708",
+  callingCode: "+880",
+};
 
-//* filtering array numbers with while loop
-var i = 0;
-while (i < someNumbers.length) {
-  if (someNumbers[i] <= 50 && someNumbers[i] >= 30) {
-    i++;
-  } else {
-    someNumbers.splice(i, 1);
-  }
-}
+var country2 = {
+  name: "India",
+  currency: "Rupee",
+  population: "1,161,376,708",
+  callingCode: "+99",
+};
 
-//* sorting array numbers
-someNumbers.sort(function (a, b) {
-  return a - b;
-});
 
-//* printing array numbers
-console.log(someNumbers);
+console.log("");
+console.log("Name : " + country1.name);
+console.log("Currency : " + country1.currency);
+console.log("Population : " + country1.population);
+console.log("Calling code : " + country1.callingCode);
+console.log("");
+console.log("Name : " + country2.name);
+console.log("Currency : " + country2.currency);
+console.log("Population : " + country2.population);
+console.log("Calling code : " + country2.callingCode);
