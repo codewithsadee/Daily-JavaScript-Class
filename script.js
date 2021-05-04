8afba3b663908be8fb48bcf139d9dab3423ebdf1
Mohammad Sadee <mohammadsadee24@gmail.com>
2021-05-04 15:43:20 +0600

    all objects types

--- script.js
+++ script.js
@@ -3,27 +3,16 @@
 const ps = require("prompt-sync");
 const prompt = ps();
 
-function DB(username, email, password) {
-  this.username = username;
+var object1 = new Object();
+object1.name = "Mohammad sadee";
+object1.email = "mohammadsadee24@gmail.com";
+
+var object2 = {
+  name: "Mohammad sadee",
+  email: "mohammadsadee24@gamil.com",
+};
+function objects(name, email) {
+  this.name = name;
   this.email = email;
-  this.password = password;
 }
-
-const user1 = new DB("tokee24", "tokeeabdullah5@gmail.com", "mdtokee892238");
-const user2 = new DB(
-  "codewithsadee",
-  "mohammadsadee24@gmail.com",
-  "mdsadee404305"
-);
-
-var userName = prompt("Username : ");
-var passWord = prompt("Password : ");
-
-if (
-  (userName == user1.username || userName == user2.username) &&
-  (passWord == user1.password || passWord == user2.password)
-) {
-  console.log("Welcome " + userName);
-} else {
-  console.log("! INFORMATION ERROR");
-}
+var object3 = new objects("Mohammad sadee", "mohammadsadee24@gamil.com");
