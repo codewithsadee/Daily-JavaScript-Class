219390877361af0eaf29fd6f0410b0c8cd930715
Mohammad Sadee <mohammadsadee24@gmail.com>
2021-05-06 22:37:07 +0600

    (max) Math object

--- script.js
+++ script.js
@@ -3,38 +3,11 @@
 const ps = require("prompt-sync");
 const prompt = ps();
 
-//* 1st way to declare object <==//
-var object1 = new Object();
-object1.name = "Mohammad sadee";
-object1.email = "mohammadsadee24@gmail.com";
-object1.print = function () {
-  console.log(object1.name);
-  console.log(object1.email);
-};
+//* max Math object class <== //
 
-//* 2nd way to declare object <==//
-var object2 = {
-  name: "Mohammad sadee",
-  email: "mohammadsadee24@gamil.com",
+var arr = [20, 30, 10, 33];
 
-  print: function () {
-    console.log(object2.name);
-    console.log(object2.email);
-  },
-};
-
-//* Object Template : creating for multiObject handling <==//
-function objects(name, email) {
-  this.name = name;
-  this.email = email;
-
-  this.print = function () {
-    console.log(this.name);
-    console.log(this.email);
-  };
+function max(num1, num2, num3, num4) {
+  return Math.max(num1, num2, num3, num4);
 }
-var object3 = new objects("Mohammad sadee", "mohammadsadee24@gamil.com");
-
-object1.print();
-object2.print();
-object3.print();
+console.log(max(arr[0], arr[1], arr[2], arr[3]));
