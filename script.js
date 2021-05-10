5d873cfb1eef816551b1ea1f6eead613756c8334
Mohammad Sadee <mohammadsadee24@gmail.com>
2021-05-11 05:08:06 +0600

    Guessing Game v1

--- script.js
+++ script.js
@@ -3,34 +3,37 @@
 const ps = require("prompt-sync");
 const prompt = ps();
 
-var winRate = 0;
-var loss = 0;
-for (var x = 1; x < 6; x++) {
-  var userGuess = parseInt(prompt("Guess the lucky number 1 to 5 : "));
-  var random = Math.floor(Math.random() * 5) + 1;
+// var score = 100;
 
-  if (random == userGuess) {
-    winRate++;
-    console.log("Exilent! your lucky number is right");
-    console.log();
+function gameObjcts() {
+  var level1 = {
+    userInput: parseInt(prompt("Guess a number 1 - 5 : ")),
+    random: Math.floor(Math.random() * 5) + 1,
+  };
+
+  if (level1.userInput == level1.random) {
+    console.log("You won!");
+    console.log("=====>Level 2<=====");
+    function gameLevelUp() {
+      var level2 = {
+        userInput: parseInt(prompt("Guess a number 1 - 10 : ")),
+        random: Math.floor(Math.random() * 10) + 1,
+      };
+      if (level2.userInput == level2.random) {
+        console.log("Congratulation You won!");
+      } else {
+        console.log("Oops you're unlucky");
+      }
+    }
+    gameLevelUp();
   } else {
-    loss++;
-    console.log("Your lucky number is wrong!");
-    console.log("Lucky Number was --> " + random);
-    console.log();
+    console.log("You've lost random number was ==> " + level1.random);
+    // score -= 20;
+    if (score == 0) {
+      console.log("Oops! you're unlucky");
+    } else {
+      gameObjcts();
+    }
   }
 }
-if (winRate > loss) {
-  console.log();
-  console.log("<===> WON! <===>");
-  console.log();
-  console.log("Right :> " + winRate + " Times");
-  console.log("Wrong :> " + loss + " Times");
-} else {
-  console.log();
-  console.log("<===> Lose! <===>");
-  console.log();
-  console.log("Right :> " + winRate + " Times");
-  console.log("Wrong :> " + loss + " Times");
-  console.log("Better luck next time!");
-}
+gameObjcts();
