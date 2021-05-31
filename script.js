d6891c3fb0b7d9f12272a1267b7b3196da7ea81b
codewithsadee <mohammadsadee24@gmail.com>
2021-05-31 11:15:28 +0600

    Creating element in javaScript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-var container = document.querySelector(".container").innerHTML += "I'm from javaScript"; //* text changing with querySelector //
-var li_tag = document.querySelectorAll("ul li a")[0].innerHTML = "I'm from javaScript"; //* text changing with querySelectorAll //
-var li_tag = document.querySelectorAll("ul li a")[1].innerHTML = "I'm from javaScript"; //* txt changing with querySelectorAll //
+//* creating new element from javaScript
+
+var Name = "It's Mohammad Sadee";
+var newHeding = document.createElement("h1");   //* creating heading one element
+var text = document.createTextNode(Name);   //* creating text for heading one
+newHeding.appendChild(text); //* adding text into heading one
+document.getElementsByClassName("my-div")[0].appendChild(newHeding);   //* adding heading element into div;
+
+
+var intro = "Hello! everyOne, I'm from javaScript";
+var newDiv = document.createElement("div");  //* creating a div element
+var newHeading1 = document.createElement("h1");  //* creating h1 element for div
+var text = document.createTextNode(intro);   //* creating text for heading
+newHeading1.appendChild(text);   //* adding text into heading
+newDiv.appendChild(newHeading1)  //* adding heading into div
+document.body.appendChild(newDiv);   //* adding div into body
