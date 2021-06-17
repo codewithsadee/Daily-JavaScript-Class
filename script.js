f98d47005475a91578a6deddba669a9f702fcfbf
codewithsadee <mohammadsadee24@gmail.com>
2021-06-18 03:58:40 +0600

    event listener

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-//* creating new element from javaScript
+// this is event listener class //
 
-var Name = "It's Mohammad Sadee";
-var newHeding = document.createElement("h1");   //* creating heading one element
-var text = document.createTextNode(Name);   //* creating text for heading one
-newHeding.appendChild(text); //* adding text into heading one
-document.getElementsByClassName("my-div")[0].appendChild(newHeding);   //* adding heading element into div;
+var elem = document.createElement("h1");
+var text = document.createTextNode("you've successfully clicked on the button");
+elem.appendChild(text);
 
+var Btn = document.querySelector(".btn");
+Btn.addEventListener("click", function () {
+  alert("hello i'm clicked");
+  document.body.appendChild(elem);
+  elem.classList.add("heading2");
+});
 
-var intro = "Hello! everyOne, I'm from javaScript";
-var newDiv = document.createElement("div");  //* creating a div element
-var newHeading1 = document.createElement("h1");  //* creating h1 element for div
-var text = document.createTextNode(intro);   //* creating text for heading
-newHeading1.appendChild(text);   //* adding text into heading
-newDiv.appendChild(newHeading1)  //* adding heading into div
-document.body.appendChild(newDiv);   //* adding div into body
