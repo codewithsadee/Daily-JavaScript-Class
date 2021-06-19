d300193f5c4daddfbc90b923b32cf0125af268c9
codewithsadee <mohammadsadee24@gmail.com>
2021-06-19 23:50:20 +0600

    eventListener with multiple element

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// this is event listener class //
-
-var elem = document.createElement("h1");
-var text = document.createTextNode("you've successfully clicked on the button");
-elem.appendChild(text);
-
-var Btn = document.querySelector(".btn");
-Btn.addEventListener("click", function () {
-  alert("hello i'm clicked");
-  document.body.appendChild(elem);
-  elem.classList.add("heading2");
-});
-
+// eventListener with multiple element //
+for (var x = 0; x < 3; x++) {
+  document.querySelectorAll(".btn")[x].addEventListener("click", function () {
+    var num = this.innerHTML;
+    document.querySelector("h2").innerHTML = num + "Clicked";
+  });
+}
