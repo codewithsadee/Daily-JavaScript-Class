fbe8c6fa4af868c07ddafe2e47b868b0a8705a78
codewithsadee <mohammadsadee24@gmail.com>
2021-06-28 05:47:15 +0600

    audio

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-// eventListener with multiple element //
-for (var x = 0; x < 3; x++) {
-  document.querySelectorAll(".btn")[x].addEventListener("click", function () {
-    var num = this.innerHTML;
-    document.querySelector("h2").innerHTML = num + "Clicked";
-  });
+// audio play //
+var buttonLength = document.querySelectorAll(".btn").length;
+
+for ( var i = 0; i < buttonLength; i++){
+	var multiSelect = document.querySelectorAll(".btn")[i];
+	multiSelect.addEventListener("click", function(){
+		var text = this.innerText;
+		console.log(text);
+		audioPlay(text);
+	});
+}
+
+function audioPlay(play){
+	switch(play){
+		case"button 1":
+			var sound = new Audio("./sounds/a.mp3");
+			sound.play();
+			break;
+		case"button 2":
+			var sound = new Audio("./sounds/b.mp3");
+			sound.play();
+			break;
+		case"button 3":
+			var sound = new Audio("./sounds/c.mp3");
+			sound.play();
+			break;
+	}
 }
